52b52b1433ee5eea4b0eb9e7341bd7732a2e70c2
Unknown <prado.app.dev@gmail.com>
2018-04-27 11:19:52 -0300

    Add Context

---
+++ MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SweetSpot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SweetSpot
+{
+    public class MyDbContext : DbContext
+    {
+        //DbSets
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Ingredients> Ingredients { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -22,6 +23,10 @@ namespace SweetSpot
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            //confugura o uso do banco
+            string connectionString = @"Data Source=LAPTOP-9E82IF5J\SQLEXPRESS;Initial Catalog=SweetSpot;Integrated Security=True";
+            services.AddDbContext<MyDbContext>(options => options.UseSqlServer(connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
